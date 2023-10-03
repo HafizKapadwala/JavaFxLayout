690fc1f2f3af1a0802ebff31c8929e8f988aa13c
MohammadHafiz Kapadwala <wbj741@usask.ca>
2023-10-02 19:56:23 -0600

    Added my rectangle class to my project

--- src/main/java/com/example/ass2/HelloApplication.java
+++ src/main/java/com/example/ass2/HelloApplication.java
@@ -3,9 +3,38 @@ package com.example.ass2;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+
+class SimWidget {
+
+    // Required values for my rectangle
+    protected double minWidth;
+    protected double maxWidth;
+    protected double prefHeight;
+
+    protected double myLeft;
+    protected double myTop;
+    protected double myWidth;
+    protected double myHeight;
+
+    protected Rectangle r;
+    public SimWidget(double minWidth, double maxWidth, double prefHeight){
+        this.minWidth = minWidth;
+        this.maxWidth = maxWidth;
+        this.prefHeight = prefHeight;
+
+        this.r = new Rectangle();
+        this.r.setFill(Color.YELLOW);
+        this.r.setStroke(Color.BLACK);
+    }
+
+
+
+}
 
 public class HelloApplication extends Application {
     @Override
